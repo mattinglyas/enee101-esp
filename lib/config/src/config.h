e2da4711cba06792a88d88ccc03735ac591f1203
Alexander Mattingly <mattinglyas@stny.rr.com>
2020-07-06 20:34:54 -0400

    add config file back

--- lib/config/src/config.h
+++ lib/config/src/config.h
@@ -1,3 +1,3 @@
-#define CONFIG_CONNECTION_STRING "HostName=amatti-iot-hub.azure-devices.net;DeviceId=MyNodeESP32;SharedAccessKey=yfLJYl2rGVwnyuJU6/yJVBVJkRZ0dJeUj+CjqW6vLag="
-#define CONFIG_WIFI_NAME "mattinglywb"
-#define CONFIG_WIFI_PASSWORD "ngws5267"
+#define CONFIG_CONNECTION_STRING ""
+#define CONFIG_WIFI_NAME ""
+#define CONFIG_WIFI_PASSWORD ""
