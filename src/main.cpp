2086356e313f137d5d98321166dc9f38442ae0fe
Alexander Mattingly <mattinglyas@stny.rr.com>
2020-07-09 15:54:07 -0400

    add queue system for commands; change logging messages

--- src/main.cpp
+++ src/main.cpp
@@ -3,14 +3,17 @@
 #include <ArduinoJson.h>
 #include <Math.h>
 #include <FreeRTOS.h>
+#include <LinkedList.h>
 #include "AzureIotHub.h"
 #include "Esp32MQTTClient.h"
 #include "config.h"
 
-#define MESSAGE_MAX_LEN 256 // size of message buffer
+#define MESSAGE_MAX_LEN 512 // size of message buffer
+#define COMMAND_BUFFER 256  // local storage queue max length
+
 #define ONBOARD_LED_PIN 2
 
-#define ULTRASOUND_X_TRIG_PIN 5 
+#define ULTRASOUND_X_TRIG_PIN 5
 #define ULTRASOUND_X_ECHO_PIN 18
 #define ULTRASOUND_Y_TRIG_PIN 16
 #define ULTRASOUND_Y_ECHO_PIN 17
@@ -26,56 +29,67 @@
 
 #define STEP_SPEED 100
 
-enum MotorCommand {
-  MOTOR_IDLE = 0, 
-  MOTOR_MOVE = 1, 
+enum MotorState
+{
+  MOTOR_IDLE = 0,
+  MOTOR_MOVE = 1,
   MOTOR_RESET = 2
 };
 
-// TODO look into wifimanager library or similar solutions to prevent wifi settings from being stored in plaintext
-const char* ssid = CONFIG_WIFI_NAME;
-const char* password = CONFIG_WIFI_PASSWORD;
+struct MotorCommand
+{
+  enum MotorState commandType;
+  int x;
+  int y;
+};
 
-// string containing HostName, Device Id & Device Key 
-static const char* connectionString = CONFIG_CONNECTION_STRING;
+// TODO look into wifimanager library or similar solutions to prevent wifi settings from being stored in plaintext
+const char *ssid = CONFIG_WIFI_NAME;
+const char *password = CONFIG_WIFI_PASSWORD;
+
+// string containing HostName, Device Id & Device Key
+static const char *connectionString = CONFIG_CONNECTION_STRING;
 
 // telemetry message
-const char* messageData = "{\"messageId\":%d, \"x_distance\":%lf, \"y_distance\":%lf}";
+const char *messageData = "{\"messageId\":%d, \"x_distance\":%lf, \"y_distance\":%lf}";
 
-int messageCount = 1; 
+int messageCount = 1;
 static long interval = 2000; //ms between telemetry messages
 static bool messageSending = true;
 static uint64_t send_interval_ms;
 static bool ledValue = false;
 
-// DEMO: STORED X AND Y VALUES, USED TO SIMULATE MOTOR LOCATION
-static int yValue = 0;
-static int xValue = 0;
+// stored x and y values
+static long yValue = 0;
+static long xValue = 0;
 
-// Shared resource for hardware and comms thread to communicate values
-static enum MotorCommand newMotorInput = MOTOR_IDLE;
-static int newMotorXInput = 0;
-static int newMotorYInput = 0;
-SemaphoreHandle_t motorMutex;
+// shared resource for hardware and comms thread to communicate values
+static LinkedList<MotorCommand> motorCommandQueue;
+static enum MotorState currentMotorState;
+SemaphoreHandle_t motorCommandMutex;
 
-// task handlers 
+// task handlers
 TaskHandle_t commsTask;
 TaskHandle_t motorTask;
 
 /* //////////////// Azure Utilities //////////////// */
 
-static void SendConfirmationCallback(IOTHUB_CLIENT_CONFIRMATION_RESULT result) {
-  if (result == IOTHUB_CLIENT_CONFIRMATION_OK) {
-    Serial.println(F("Send Confirmation Callback finished."));
+static void SendConfirmationCallback(IOTHUB_CLIENT_CONFIRMATION_RESULT result)
+{
+  if (result == IOTHUB_CLIENT_CONFIRMATION_OK)
+  {
+    Serial.println(F("Info: Send Confirmation Callback finished"));
   }
 }
 
-static void MessageCallback(const char* payload, int size) {
-  Serial.println(F("Message callback:"));
+static void MessageCallback(const char *payload, int size)
+{
+  Serial.println(F("Info: Message callback:"));
   Serial.println(payload);
 }
 
-static void DeviceTwinCallback(DEVICE_TWIN_UPDATE_STATE updateState, const unsigned char* payload, int size) {
+static void DeviceTwinCallback(DEVICE_TWIN_UPDATE_STATE updateState, const unsigned char *payload, int size)
+{
   char *temp = (char *)malloc(size + 1);
   if (temp == NULL)
   {
@@ -88,56 +102,79 @@ static void DeviceTwinCallback(DEVICE_TWIN_UPDATE_STATE updateState, const unsig
   free(temp);
 }
 
-static int DeviceMethodCallback(const char* methodName, const unsigned char* payload, int size, unsigned char** response, int* response_size) {
-  LogInfo("Try to invoke method %s", methodName);
+static int DeviceMethodCallback(const char *methodName, const unsigned char *payload, int size, unsigned char **response, int *response_size)
+{
+  Serial.print("Info: Trying to invoke method ");
+  Serial.println(methodName);
   const char *responseMessage = "{\"status\":200}";
   int result = 200;
 
   StaticJsonDocument<MESSAGE_MAX_LEN> doc;
-  deserializeJson(doc,payload,MESSAGE_MAX_LEN);
-  Serial.println(F("Received payload:"));
-  serializeJsonPretty(doc, Serial);
+  deserializeJson(doc, payload, MESSAGE_MAX_LEN);
+  Serial.print(F("Info: Received payload: "));
+  serializeJson(doc, Serial);
   Serial.println();
 
-  if (strcmp(methodName, "start") == 0) {
-    LogInfo("Start sending coordinate data");
+  if (strcmp(methodName, "start") == 0)
+  {
     messageSending = true;
+    Serial.println(F("Info: Started sending telemetry messages"));
   }
-  else if (strcmp(methodName, "stop") == 0) {
-    LogInfo("Stop sending coordinate data");
+  else if (strcmp(methodName, "stop") == 0)
+  {
     messageSending = false;
-  } else if (strcmp(methodName, "led") == 0) {
-    LogInfo("Toggling on-board LED");
+    Serial.println(F("Info: Stopped sending telemetry messages"));
+  }
+  else if (strcmp(methodName, "led") == 0)
+  {
     ledValue = !ledValue;
     digitalWrite(ONBOARD_LED_PIN, ledValue);
-  } else if (strcmp(methodName, "interval") == 0) {
-    LogInfo("Changing telemetry interval time");
-
+    Serial.println(F("Info: Toggled on-board LED"));
+  }
+  else if (strcmp(methodName, "interval") == 0)
+  {
     JsonVariant newInterval = doc["interval"];
     interval = newInterval.as<int>();
-  } else if (strcmp(methodName, "move") == 0) {
-    LogInfo("Moving motors");
-
+    Serial.print(F("Info: Changed telemetry interval to "));
+    Serial.print(interval);
+    Serial.println(F("ms"));
+  }
+  else if (strcmp(methodName, "move") == 0)
+  {
     JsonVariant newXValue = doc["x"];
     JsonVariant newYValue = doc["y"];
 
-    // pass new values into shared space, set flag that new data is available
-    xSemaphoreTake(motorMutex, portMAX_DELAY);
-    newMotorInput = MOTOR_MOVE;
-    newMotorXInput = newXValue.as<int>();
-    newMotorYInput = newYValue.as<int>();
-    xSemaphoreGive(motorMutex);
-  } else if (strcmp(methodName, "reset") == 0) {
-    LogInfo("Resetting motors");
+    // pass new values into shared space
+    xSemaphoreTake(motorCommandMutex, portMAX_DELAY);
+    struct MotorCommand newCommand;
+    newCommand.commandType = MOTOR_MOVE;
+    newCommand.x = newXValue.as<int>();
+    newCommand.y = newYValue.as<int>();
+    motorCommandQueue.add(newCommand);
+    xSemaphoreGive(motorCommandMutex);
 
-    // pass new values into shared space, set flag that new data is available
-    xSemaphoreTake(motorMutex, portMAX_DELAY);
-    newMotorInput = MOTOR_RESET;
-    xSemaphoreGive(motorMutex);
-  } else {
-    LogInfo("No method %s found", methodName);
+    Serial.print(F("Info: Queued move command to coordinates (x,y) = "));
+    Serial.print(newCommand.x);
+    Serial.print(F(","));
+    Serial.println(newCommand.y);
+  }
+  else if (strcmp(methodName, "reset") == 0)
+  {
+    // pass new values into shared space
+    xSemaphoreTake(motorCommandMutex, portMAX_DELAY);
+    struct MotorCommand newCommand;
+    newCommand.commandType = MOTOR_RESET;
+    motorCommandQueue.add(newCommand);
+    xSemaphoreGive(motorCommandMutex);
+
+    Serial.println(F("Info: Queued reset command"));
+  }
+  else
+  {
     responseMessage = "{\"status\":404}";
     result = 404;
+
+    Serial.println(F("Warning: Received invalid command"));
   }
 
   *response_size = strlen(responseMessage) + 1;
@@ -148,23 +185,28 @@ static int DeviceMethodCallback(const char* methodName, const unsigned char* pay
 
 /* //////////////// Device Utilities //////////////// */
 
-static bool InitWifi() {
-  Serial.print(F("Connecting to ")); Serial.println(ssid);
+static bool InitWifi()
+{
+  Serial.print(F("Info: Connecting to "));
+  Serial.println(ssid);
   WiFi.begin(ssid, password);
-  
+
   // hang while not connected (bit of a hack)
-  // TODO make connection time out, return with hasWifi false 
-  do {
+  // TODO make connection time out, return with hasWifi false
+  do
+  {
     delay(500);
     Serial.print(".");
   } while (WiFi.status() != WL_CONNECTED);
 
-  Serial.println(F("WiFi connected"));
-  Serial.print(F("IP Address: ")); Serial.println(WiFi.localIP());
+  Serial.println();
+  Serial.print(F("Info: WiFi connected. Local IP Address: "));
+  Serial.println(WiFi.localIP());
   return true;
 }
 
-static double GetUltrasoundDistanceInInches(int trigPin, int echoPin) {
+static double GetUltrasoundDistanceInInches(int trigPin, int echoPin)
+{
   // pinModes are necessary for some reason. Code does not work without them
 
   long duration;
@@ -176,57 +218,66 @@ static double GetUltrasoundDistanceInInches(int trigPin, int echoPin) {
   digitalWrite(trigPin, LOW);
   pinMode(echoPin, INPUT);
   duration = pulseIn(echoPin, HIGH);
-  return ((double) duration) / 74 / 2;  
+  return ((double)duration) / 74 / 2;
 }
 
-static void ResetMotors() {
-    digitalWrite(MOTOR_X_DIR_PIN, LOW);
-    digitalWrite(MOTOR_Y_DIR_PIN, HIGH);
-    while(xValue > 0 || yValue > 0) {
-      if (xValue > 0) {
-        digitalWrite(MOTOR_X_STEP_PIN, HIGH);
-        xValue--;
-      }
-      if (yValue > 0) {
-        digitalWrite(MOTOR_Y_STEP_PIN, HIGH);
-        yValue--;
-      }
-      delayMicroseconds(STEP_SPEED);
-      digitalWrite(MOTOR_X_STEP_PIN, LOW);
-      digitalWrite(MOTOR_Y_STEP_PIN, LOW);
-      delayMicroseconds(STEP_SPEED);
+static void ResetMotors()
+{
+  digitalWrite(MOTOR_X_DIR_PIN, LOW);
+  digitalWrite(MOTOR_Y_DIR_PIN, HIGH);
+  while (xValue > 0 || yValue > 0)
+  {
+    if (xValue > 0)
+    {
+      digitalWrite(MOTOR_X_STEP_PIN, HIGH);
+      xValue--;
     }
+    if (yValue > 0)
+    {
+      digitalWrite(MOTOR_Y_STEP_PIN, HIGH);
+      yValue--;
+    }
+    delayMicroseconds(STEP_SPEED);
+    digitalWrite(MOTOR_X_STEP_PIN, LOW);
+    digitalWrite(MOTOR_Y_STEP_PIN, LOW);
+    delayMicroseconds(STEP_SPEED);
+  }
 }
 
-static void MoveMotors(int xxx, int yyy) {
+static void MoveMotors(int xxx, int yyy)
+{
   unsigned long ctrstepx = 0;
   unsigned long ctrstepy = 0;
 
-  int flagsx = 0;                    // assigning sign to x value input. 0 means positive 1 means negative
-  int flagsy = 0;                    // assigning sign to y value input. 0 means positive 1 means negative
+  int flagsx = 0; // assigning sign to x value input. 0 means positive 1 means negative
+  int flagsy = 0; // assigning sign to y value input. 0 means positive 1 means negative
 
-  if(xxx > 0)                
-    digitalWrite(MOTOR_X_DIR_PIN, HIGH);     // setting x motor direction according to input
-  if(xxx < 0) {
-    digitalWrite(MOTOR_X_DIR_PIN, LOW);      // setting x motor direction according to input
+  if (xxx > 0)
+    digitalWrite(MOTOR_X_DIR_PIN, HIGH); // setting x motor direction according to input
+  if (xxx < 0)
+  {
+    digitalWrite(MOTOR_X_DIR_PIN, LOW); // setting x motor direction according to input
     xxx = abs(xxx);
     flagsx = 1;
   }
-  if(yyy > 0)
-    digitalWrite(MOTOR_Y_DIR_PIN, LOW);      // setting y motor direction according to input
-  if(yyy < 0) {
-    digitalWrite(MOTOR_Y_DIR_PIN, HIGH);     // setting y motor direction according to input
+  if (yyy > 0)
+    digitalWrite(MOTOR_Y_DIR_PIN, LOW); // setting y motor direction according to input
+  if (yyy < 0)
+  {
+    digitalWrite(MOTOR_Y_DIR_PIN, HIGH); // setting y motor direction according to input
     yyy = abs(yyy);
     flagsy = 1;
   }
 
   while (ctrstepx <= xxx || ctrstepy <= yyy)
   {
-    if (ctrstepx <= xxx) {
+    if (ctrstepx <= xxx)
+    {
       digitalWrite(MOTOR_X_STEP_PIN, HIGH);
       ctrstepx++;
     }
-    if (ctrstepy <= yyy) {
+    if (ctrstepy <= yyy)
+    {
       digitalWrite(MOTOR_Y_STEP_PIN, HIGH);
       ctrstepy++;
     }
@@ -236,40 +287,33 @@ static void MoveMotors(int xxx, int yyy) {
     delayMicroseconds(STEP_SPEED);
   }
 
-  if(flagsx == 1)               // flags[ign]x is determined in the function input(), it is to keep track of the sign
-    xValue = xValue - ctrstepx;     // of the input.
-  if(flagsx == 0)
+  if (flagsx == 1)              // flags[ign]x is determined in the function input(), it is to keep track of the sign
+    xValue = xValue - ctrstepx; // of the input.
+  if (flagsx == 0)
     xValue = xValue + ctrstepx;
-    ctrstepx = 0;
-    
-  if(flagsy == 1)               // flags[ign]y is determined in the function input(), it is to keep track of the sign
-    yValue = yValue - ctrstepy;     // of the input.
-  if(flagsy == 0)
+  ctrstepx = 0;
+
+  if (flagsy == 1)              // flags[ign]y is determined in the function input(), it is to keep track of the sign
+    yValue = yValue - ctrstepy; // of the input.
+  if (flagsy == 0)
     yValue = yValue + ctrstepy;
   ctrstepy = 0;
 }
 
-int max(int x, int y) { 
-  return (x > y) ? x : y;
-}
-int min(int x, int y) { 
-  return (x < y) ? x : y;
-}
-
 /* //////////////// Tasks //////////////// */
 
-static void CommsTask(void* pvParameters) {
-  Serial.print("Starting messaging task on core ");
+static void CommsTask(void *pvParameters)
+{
+  Serial.print(F("Info: Starting messaging task on core "));
   Serial.println(xPortGetCoreID());
 
   // initialize wifi module
-  Serial.println(F("> WiFi"));
   bool hasWifi = InitWifi();
-  if (!hasWifi) return;
+  if (!hasWifi)
+    return;
 
-  Serial.println(F(" > IoT Hub"));
-  Esp32MQTTClient_SetOption(OPTION_MINI_SOLUTION_NAME, "GetStarted");
-  Esp32MQTTClient_Init((const uint8_t*)connectionString, true);
+  Esp32MQTTClient_SetOption(OPTION_MINI_SOLUTION_NAME, "ENEE101-ESP32");
+  Esp32MQTTClient_Init((const uint8_t *)connectionString, true);
 
   Esp32MQTTClient_SetSendConfirmationCallback(SendConfirmationCallback);
   Esp32MQTTClient_SetMessageCallback(MessageCallback);
@@ -278,9 +322,12 @@ static void CommsTask(void* pvParameters) {
 
   send_interval_ms = millis();
 
-  while (true) {
-    if (hasWifi) {
-      if (messageSending && (int)(millis() - send_interval_ms) >= interval) {
+  while (true)
+  {
+    if (hasWifi)
+    {
+      if (messageSending && (int)(millis() - send_interval_ms) >= interval)
+      {
         char messagePayload[MESSAGE_MAX_LEN];
 
         /* DEMO: RANDOM VALUES, REPLACE WITH SENSOR CODE */
@@ -292,18 +339,15 @@ static void CommsTask(void* pvParameters) {
 
         // copy into message
         snprintf(messagePayload, MESSAGE_MAX_LEN, messageData, messageCount++, xDistance, yDistance);
-      
-        Serial.println(F("Sending message: "));
+
+        Serial.print(F("Info: Sending message to server "));
         Serial.println(messagePayload);
 
-        EVENT_INSTANCE* message = Esp32MQTTClient_Event_Generate(messagePayload, MESSAGE);
+        EVENT_INSTANCE *message = Esp32MQTTClient_Event_Generate(messagePayload, MESSAGE);
         Esp32MQTTClient_SendEventInstance(message);
 
-        Serial.println(F("------------------"));
-        Serial.println();
-
         send_interval_ms = millis();
-      } 
+      }
 
       Esp32MQTTClient_Check();
     }
@@ -313,68 +357,77 @@ static void CommsTask(void* pvParameters) {
   }
 }
 
-static void MotorTask(void* pvParameters) {
-  Serial.print("Starting motor task on core ");
+static void MotorTask(void *pvParameters)
+{
+  Serial.print(F("Info: Starting motor task on core "));
   Serial.println(xPortGetCoreID());
 
-  // task-specific copy of new x and y values
-  int x;
-  int y;
+  // task-specific copy of new command
+  MotorCommand command;
 
-  while (true) {
-    // TODO implement thread sleep/wake instead of using flags to communicate new data
+  // task-specific copy of queue size
+  int commandQueueLength;
+
+  while (true)
+  {
     // check for new data from communications thread
-    xSemaphoreTake(motorMutex, portMAX_DELAY);
-    switch (newMotorInput) {
-      case MOTOR_IDLE:
-      {
-        xSemaphoreGive(motorMutex);
-        vTaskDelay(100);
-        break;
-      }
-      case MOTOR_MOVE:
-      {
-        // copy data from shared resource into task-specific copy; reset input flag
-        newMotorInput = MOTOR_IDLE;
-        x = newMotorXInput;
-        y = newMotorYInput;
-        xSemaphoreGive(motorMutex);
-      
-        Serial.print(F("Moving (x,y): ("));
-        Serial.print(x);
-        Serial.print(",");
-        Serial.print(y);
-        Serial.println(")");
+    xSemaphoreTake(motorCommandMutex, portMAX_DELAY);
+    commandQueueLength = motorCommandQueue.size();
+    xSemaphoreGive(motorCommandMutex);
 
-        MoveMotors(x,y);
+    while (commandQueueLength > 0)
+    {
+      // new data is available, copy first command
+      xSemaphoreTake(motorCommandMutex, portMAX_DELAY);
+      command = motorCommandQueue.shift();
+      commandQueueLength = motorCommandQueue.size();
+      xSemaphoreGive(motorCommandMutex);
 
-        Serial.println(F("Move finished"));
-        break;
-      }
-      case MOTOR_RESET:
-      {
-        // reset input flag
-        newMotorInput = MOTOR_IDLE;
-        xSemaphoreGive(motorMutex);
-        Serial.println("Resetting motors");
+      currentMotorState = command.commandType;
 
-        ResetMotors();
-        
-        Serial.println("Reset finished");
-        break;
-      }
-      default: 
+      switch (command.commandType)
       {
-        xSemaphoreGive(motorMutex);
-        break;
+        case MOTOR_IDLE:
+        {
+          // should not get here
+          break;
+        }
+        case MOTOR_MOVE:
+        {
+          Serial.print(F("Info: Moving motors to x,y = "));
+          Serial.print(command.x);
+          Serial.print(F(","));
+          Serial.println(command.y);
+          
+          MoveMotors(command.x, command.y);
+
+          Serial.println(F("Info: Move finished"));
+          break;
+        }
+        case MOTOR_RESET:
+        {
+          Serial.println(F("Info: Resetting motors"));
+          ResetMotors();
+          Serial.println(F("Info: Reset finished"));
+          break;
+        }
+        default:
+        {
+          // should not get here
+          break;
+        }
       }
     }
+
+    currentMotorState = MOTOR_IDLE;
+    vTaskDelay(100);
   }
 }
 
 /* //////////////// Arduino Sketch //////////////// */
 
-void setup() {
+void setup()
+{
   pinMode(ULTRASOUND_X_ECHO_PIN, INPUT);
   pinMode(ULTRASOUND_X_TRIG_PIN, OUTPUT);
   pinMode(ULTRASOUND_Y_ECHO_PIN, INPUT);
@@ -386,7 +439,7 @@ void setup() {
   pinMode(ONBOARD_LED_PIN, OUTPUT);
   pinMode(CLK_PIN, INPUT);
   pinMode(DAT_PIN, INPUT);
-  
+
   digitalWrite(ULTRASOUND_X_TRIG_PIN, LOW);
   digitalWrite(ULTRASOUND_Y_TRIG_PIN, LOW);
   digitalWrite(MOTOR_X_DIR_PIN, LOW);
@@ -400,28 +453,27 @@ void setup() {
   Serial.println(F("ESP32 Device"));
   Serial.println(F("Initializing..."));
 
-  motorMutex = xSemaphoreCreateMutex();
+  motorCommandMutex = xSemaphoreCreateMutex();
 
   xTaskCreatePinnedToCore(
-    CommsTask,
-    "Comms Task",
-    65536,
-    NULL,
-    1,
-    &commsTask,
-    0
-  );
+      CommsTask,
+      "Comms Task",
+      65536,
+      NULL,
+      1,
+      &commsTask,
+      0);
 
   xTaskCreatePinnedToCore(
-    MotorTask,
-    "Motor Task",
-    16384,
-    NULL,
-    1,
-    &motorTask,
-    1
-  );
+      MotorTask,
+      "Motor Task",
+      16384,
+      NULL,
+      1,
+      &motorTask,
+      1);
 }
 
-void loop() {
+void loop()
+{
 }
