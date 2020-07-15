f030bd28a72de98d33e8da66323728604b5491ef
Alexander Mattingly <mattinglyas@stny.rr.com>
2020-07-15 13:25:00 -0400

    move config

--- lib/config/src/config.h
+++ lib/config/src/config.h
@@ -1,3 +1,3 @@
-#define CONFIG_CONNECTION_STRING "HostName=amatti-iot-hub.azure-devices.net;DeviceId=MyNodeESP32;SharedAccessKey=zEyEkUkz2L7CsgH74O7K+w7ktq6sr/DC6majfrgAaDs="
-#define CONFIG_WIFI_NAME "mattinglywb"
-#define CONFIG_WIFI_PASSWORD "ngws5267"
+#define CONFIG_CONNECTION_STRING ""
+#define CONFIG_WIFI_NAME ""
+#define CONFIG_WIFI_PASSWORD ""
